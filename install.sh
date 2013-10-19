f023fa35bb79374b3f7e0bc541e25c4e783d3dd5
Troy Denton <trdenton@gmail.com>
2013-10-19 01:04:46 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@
 
 echo "Make sure to run this with sudo!"
 
+mkdir -p /usr/local/bin
 cp ./dingle /usr/local/bin/
 cp ./dingle.wav /usr/local/
+chmod a+x /usr/local/bin/dingle
+chmod a+x /usr/local/dingle.wav
