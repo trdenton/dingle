0d17e71746b9940729b36d9831ecb59920221b0f
Troy Denton <trdenton@gmail.com>
2013-10-20 23:48:04 -0500

    got it working by changing shell script to a bash function.  now, only compatable with bash however.  need to test further

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-#must run as sudo!
+#likely only compatable with bash
+#make sure to perform a "source ~\.bashrc" afterward!
+echo 'make sure to perform a "source ~\.bashrc"! '
 
-echo "Make sure to run this with sudo!"
 
-mkdir -p /usr/local/bin
-cp ./dingle /usr/local/bin/
-cp ./dingle.wav /usr/local/
-chmod a+x /usr/local/bin/dingle
-chmod a+r /usr/local/dingle.wav
+mkdir -p ~/.dingle
+cp ./dingle.wav ~/.dingle/dingle.wav
+chmod a+r ~/.dingle/dingle.wav
+
+
+grep 'dingle()' ~/.bashrc > /dev/null
+if [ $? -eq 1 ]; then
+	echo 'function dingle() { "$@"; aplay ~/.dingle/dingle.wav 2> /dev/null; }' >> ~/.bashrc
+fi;
